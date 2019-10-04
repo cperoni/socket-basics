ea1b06c3524d9889547be786c7814d7293408840
christianperoni <christian.peroni@gmail.com>
2019-10-04 15:30:12 +0200

    aggiunta evento per messagi custom tra 2 browser

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function () {
     console.log('connected to socket io server');
 });
+
+socket.on('message', function (message) {
+    console.log('New msg:');
+    console.log(message.text);
+
+});
