5e65e180eff1b62dcac8bdee564a92d1520ad7cc
christianperoni <christian.peroni@gmail.com>
2019-10-04 13:08:08 +0200

    aggiunto Socket.io a client e server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function () {
+    console.log('User connesso via socket io');
+})
 http.listen(PORT, function () {
     console.log('server started');
 });
+
