c49ac65f831c3d78ff2cffd71b1f6f128a9a9d11
christianperoni <christian.peroni@gmail.com>
2019-10-07 17:12:32 +0200

    aggiunta front-end per submit messagi

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,14 @@ socket.on('message', function (message) {
     console.log(message.text);
 
 });
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+    event.preventDefault();
+    var $message = $form.find('input[name=message]');
+    socket.emit('message', {
+        text: $message.val()
+    });
+    $message.val('');
+})
