643b7bfd3a13152beebb0cd866cfd4e6b7d08b71
christianperoni <christian.peroni@gmail.com>
2019-10-08 17:02:59 +0200

    add disconnect event and message

--- server.js
+++ server.js
@@ -11,13 +11,28 @@ var clientInfo = {};
 io.on('connection', function (socket) {
     console.log('User connesso via socket io');
 
+    socket.on('disconnect', function () {
+
+        const userData = clientInfo[socket.id];
+
+        if (typeof userData !== undefined) {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: 'System',
+                text: userData.name + ' has left!',
+                timestamp: moment().valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
+    });
+
     socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
         socket.broadcast.to(req.room).emit('message', {
             name: 'System',
             text: req.name + ' has joined!',
-            timestamp: moment.valueOf()
+            timestamp: moment().valueOf()
         });
     });
 
